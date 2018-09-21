fb09c3f3538cf06057dc20138efa12db1f78c323
Lab.Academico <Lab.Academico@CET-LAB05PC30.unisul.br>
2018-09-20 21:47:24 -0300

    yo

--- Battle/src/battle/Main.java
+++ Battle/src/battle/Main.java
@@ -1,7 +1,29 @@
 package battle;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
-		Player warrior = new Player(); 
+		Player warrior = new Player("Gort");
+		Player elf = new Player("Berzeron");
+		warrior.setMhp(dice(2,4,12));
+		warrior.setHp(warrior.getMhp());
+		warrior.setMmp(dice(1,4,4));
+		warrior.setMp(warrior.getMmp());
+		
+		elf.setMhp(dice(1,6,8));
+		elf.setHp(elf.getMhp());
+		elf.setMmp(dice(2,4,6));
+		elf.setMp(elf.getMmp());
+		System.out.println(warrior);
+		System.out.println(elf);
+	}
+	
+	public static int dice(int count, int die, int constant) {
+		Random r = new Random();
+		int result = 0;
+		for(int i = 0; i < count; i++)
+			result += r.nextInt(die) + 1; 
+		return result + constant;
 	}
 }
--- Battle/src/battle/Player.java
+++ Battle/src/battle/Player.java
@@ -3,10 +3,67 @@ package battle;
 import java.util.ArrayList;
 
 public class Player {
+	private String name;
+	
 	private int hp;
 	private int mhp;
 	private int mp;
 	private int mmp;
 	private ArrayList<Effect> effects;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	private String getName() {
+		return name;
+	}
+	
+	public int getHp() {
+		return hp;
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getMhp() {
+		return mhp;
+	}
+	
+	public void setMhp(int mhp) {
+		this.mhp = mhp;
+	}
+	
+	public int getMp() {
+		return mp;
+	}
+	
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+	
+	public int getMmp() {
+		return mmp;
+	}
+	
+	public void setMmp(int mmp) {
+		this.mmp = mmp;
+	}
+	
+	public ArrayList<Effect> getEffects() {
+		return effects;
+	}
+	
+	public void setEffects(ArrayList<Effect> effects) {
+		this.effects = effects;
+	}
+	
+	@Override
+	public String toString() {
+		return getName() + ": HP " + getHp() + "/" + getMhp() + " MP " 
+				+ getMp() + "/" + getMmp();
+	}
+
 	
 }
