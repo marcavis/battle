95affb4699cc3a4310193d04edfb5c619065df93
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2018-09-30 03:13:13 -0300

    start from vterminal demo

--- Battle/src/battle/Driver.java
+++ Battle/src/battle/Driver.java
@@ -1,31 +1,17 @@
 package battle;
 
 import com.valkryst.VTerminal.Screen;
-import com.valkryst.VTerminal.Tile;
 
-import java.awt.Color;
 import java.io.IOException;
 
 public class Driver {
-    public static void main(String[] args) throws IOException {
-        final Screen screen = new Screen(80,24);
-        screen.addCanvasToFrame();
+	public static void main(String[] args) throws IOException {
+		final Screen screen = new Screen(80, 40);
+		screen.addCanvasToFrame();
 
-        for(int x = 0; x < screen.getWidth(); x++) {
-        	for(int y = 0; y < screen.getHeight(); y++) {
-        		screen.getTileAt(x, y).setCharacter('1');
-        	}
-        }
+		final Map map = new Map();
+		screen.addComponent(map);
 
-        Tile[] helloTiles = screen.getTiles().getRowSubset(0, 0, 12);
-
-        for (final Tile tile : helloTiles) {
-            tile.setBackgroundColor(Color.BLACK);
-            tile.setForegroundColor(Color.WHITE);
-        }
-        
-        
-        
-        screen.draw();
-    }
+		screen.draw();
+	}
 }
